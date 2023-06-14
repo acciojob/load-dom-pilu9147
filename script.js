39546d39188610a6be690df00415d91af13036b6
pilu9147 <119696172+pilu9147@users.noreply.github.com>
2023-06-14 09:50:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //your JS code here. If required.
 let div = document.getElementsByTagName("div")[0];
-window.addEventListener(('load',()=>{
+function onLo(){
 	div.innerHTML = "DOM load success";
-}))
+})
+window.addEventListener(('load',onLo)
